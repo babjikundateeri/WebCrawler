968229f88e8d25158eda1888d59d1bf4d0121696
babjikundateeri <babji.kundateeri@gmail.com>
2016-04-05 11:34:57 +0530

    initial commit

---
+++ src/main/java/com/pramati/imaginea/webCrawler/WebCrawler/App.java
@@ -0,0 +1,13 @@
+package com.pramati.imaginea.webCrawler.WebCrawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
